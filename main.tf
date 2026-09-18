ef89115f078bfbc2b916ca6731dd0f5d06be1567
deysur <surajitdey000@gmail.com>
2026-09-18 08:29:53 +0530

    update project

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
-data "aws_ami" "amazon_linux" { 
-    most_recent = true 
-    owners = ["amazon"] 
-    filter { 
-        name = "name" 
-        values = ["amzn2-ami-hvm--x86_64-gp2"] 
-    } 
-    } 
-resource "aws_instance" "web" { 
-    count = var.instance_count 
-    ami = data.aws_ami.amazon_linux.id 
-    instance_type = var.instance_type 
-    subnet_id = var.subnet_ids[count.index % length(var.subnet_ids)]
-    tags = { 
-        Name = "web-${var.environment}-${count.index}" 
-        Environment = var.environment 
-        } 
-        } 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm--x86_64-gp2"]
+  }
+}
+resource "aws_instance" "web" {
+  count         = var.instance_count
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_ids[count.index % length(var.subnet_ids)]
+  tags = {
+    Name        = "web-${var.environment}-${count.index}"
+    Environment = var.environment
+  }
+}
