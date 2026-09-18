87b59af2a60a2fe4b41ee91e81585b370a209836
deysur <surajitdey000@gmail.com>
2026-09-18 08:27:48 +0530

    update project

---
+++ dev.tfvars
@@ -0,0 +1,5 @@
+environment    = "dev"
+instance_type  = "t2.micro"
+instance_count = 1
+vpc_id         = "vpc-08357a6f511fc7d9e"
+subnet_ids     = ["subnet-0103ba51b7736ab04", "subnet-0aa02cb5a77d3902b"]
--- main.tf
+++ main.tf
@@ -10,12 +10,9 @@ resource "aws_instance" "web" {
     count = var.instance_count 
     ami = data.aws_ami.amazon_linux.id 
     instance_type = var.instance_type 
-    subnet_id = var.subnet_ids[count.index % length(var.subnet_ids)] 
+    subnet_id = var.subnet_ids[count.index % length(var.subnet_ids)]
     tags = { 
         Name = "web-${var.environment}-${count.index}" 
         Environment = var.environment 
         } 
         } 
-output "web_ips" { 
-    value = aws_instance.web[].public_ip 
-    }
