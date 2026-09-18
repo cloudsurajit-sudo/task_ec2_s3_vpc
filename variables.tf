a8619dbcdeebaeee68835df75b34ce49dbeeddf6
deysur <surajitdey000@gmail.com>
2026-09-18 08:27:24 +0530

    update project

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
-variable "environment" { 
-    type = string 
-    } 
-variable "instance_type" { 
-    type = string 
-    } 
-variable "instance_count" { 
-    type = number 
-    } 
-variable "vpc_id" { 
-    type = string 
-    } 
-variable "subnet_ids" { 
-    type = list(string) 
-    }
+variable "environment" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
+variable "instance_count" {
+  type = number
+}
+variable "vpc_id" {
+  type = string
+}
+variable "subnet_ids" {
+  type = list(string)
+}
